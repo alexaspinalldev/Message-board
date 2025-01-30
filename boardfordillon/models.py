8460501aca6bc41179ec572adb7d843bb4634002
Jude Woolls <jwoolls01@gmail.com>
2025-01-30 14:34:00 +0000

    add models for post, comment and reactions

--- boardfordillon/models.py
+++ boardfordillon/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+    
+reactions = ['like', 'dislike', 'love', 'funny', 'wow', 'sad', 'angry']
+
+class Reaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    reaction = models.Choices(reactions)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reaction
