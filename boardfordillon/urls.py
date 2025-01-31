26cf678bf08ea8fa2530587c20a9ead4f28dc4a4
jess <jess.howkins@yahoo.com>
2025-01-31 14:11:09 +0000

    form working, no log in details as yet

--- boardfordillon/urls.py
+++ boardfordillon/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.MessageList.as_view(), name='Home'),
+    path('home/', views.MessageList.as_view(), name='home'),
     path('post_form/', views.post_form, name='post_form')
 ]
