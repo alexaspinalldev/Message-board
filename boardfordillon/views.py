1786d15ef2bda2b5fb6d0a07b98773cc3fd942b9
jess <jess.howkins@yahoo.com>
2025-01-31 13:30:40 +0000

    removed duplicate forms.py

--- boardfordillon/views.py
+++ boardfordillon/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post, Comment, Reaction
-from .forms import PostForm
+# from .forms import PostForm
 
 
 # Create your views here.
 class MessageList(generic.ListView):
     model = Post
     template_name = "home.html"
-
-
-# ??
-@login_required
-def post_form(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('home')
-    else:
-        form = PostForm()
-    return render(
-        request, 'home.html', {'form': form, 'object_list': Post.objects.all().order_by(
-            '-date_posted')})
-
