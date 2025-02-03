ea9167aceeb042b99599d7e72447c1f7d1731cad
jess <jess.howkins@yahoo.com>
2025-02-03 16:47:31 +0000

    final commit and media queries

--- messageboard/settings.py
+++ messageboard/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-a(x%$hv&=5vre1ix1g%4^5lr+w-n25oec%t&chcm*d-k3m7xc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  '.herokuapp.com',
